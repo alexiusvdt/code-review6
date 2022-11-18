d4e672c3f43df472f746d5e7782b3012d701003a
Alex Johnson <alex.johnson293@gmail.com>
2022-11-18 10:23:54 -0800

    WIP: alternate call also returning CORS, API possibly updated since lesson was originally written

--- src/index.js
+++ src/index.js
@@ -5,39 +5,39 @@ import ExchangeService from './exchange-service.js';
 
 // Business Logic
 
-async function getInput(currIn, currOut, amount) {
-  const response = await ExchangeService.exchangeMoney(currIn, currOut, amount);
+async function getInput(currIn) {
+  const response = await ExchangeService.exchangeMoney(currIn);
   if (response.main) {
-    printElements(response, currIn, currOut, amount);
+    printElements(response, currIn);
   } else {
-    printError(response, currIn, currOut, amount);
+    printError(response, currIn);
   }
 }
 
 // UI Logic
 
-function printElements(response, currIn, currOut, amount) {
-  document.querySelector('#showResponse').innerText = `${currIn} has an exchange rate of ${response.conversion_rate} to ${currOut}.\n
-  Your ${amount} ${currIn} will buy ${response.conversion_result} ${currOut}.`;
+function printElements(response, currIn) {
+  document.querySelector('#showResponse').innerText = `${currIn} has an exchange rate of ${response.conversion_rate}`
+  //  to ${currOut}.\n
+  // Your ${amount} ${currIn} will buy ${response.conversion_result} ${currOut}.`;
 }
 
-function printError(error, currIn, currOut) {
-  document.querySelector('#showResponse').innerText = `There was an error in your request for ${currIn} to ${currOut}: 
+function printError(error, currIn) {
+  document.querySelector('#showResponse').innerText = `There was an error in your request for ${currIn}: 
   ${error}.`;
 }
 
 function handleFormSubmission(event) {
   event.preventDefault();
-  const amount = document.querySelector('#amount').value;
+  // const amount = document.querySelector('#amount').value;
   const from = document.getElementById('currIn');
-  const to = document.getElementById('currOut');
+  // const to = document.getElementById('currOut');
   const currIn = from.options[from.selectedIndex].value;
-  const currOut = to.options[to.selectedIndex].value;
+  // const currOut = to.options[to.selectedIndex].value;
   document.querySelector('#currIn').value = null;
-  document.querySelector('#currOut').value = null;
-  getInput(currIn, currOut, amount);
+  // document.querySelector('#currOut').value = null;
+  getInput(currIn);
 }
-// size.options[size.selectedIndex].value
 
 window.addEventListener("load", function() {
   document.querySelector('form').addEventListener("submit", handleFormSubmission);
