3ab4ceb1bd7dc63228775040f6876bd23b3324fe
Alex Johnson <alex.johnson293@gmail.com>
2022-11-18 10:40:10 -0800

    main branch call missing all inputs

--- src/exchange-service.js
+++ src/exchange-service.js
@@ -1,7 +1,7 @@
-export default class MoneyService {  
+export default class ExchangeService {  
   static async exchangeMoney(currIn, currOut, amount) {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair${currIn}/${currOut}/${amount}`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currIn}/${currOut}/${amount}`);
       const jsonResponse = await response.json();
       if (!response.ok) {
         const errorMessage = `${response.status} ${response.statusText}
